1ec737f2e795306ce230e366278a09efa076b4fa
Juri Semjonov <juri.semjonov@gmail.com>
2012-10-26 11:13:34 +0300

    Fix for omniauth

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, touch: true
   attr_accessible :name, :uid
 
   validates :name, :presence => true, :inclusion => { in: User.omniauth_providers.map{ |i|i.to_s } }
