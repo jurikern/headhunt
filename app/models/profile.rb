a31c55c45e1c2ce28639d0f57b60acdfccbac5dc
Juri Semjonov <juri.semjonov@gmail.com>
2012-11-04 00:12:21 +0200

    loofah for description

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,11 @@
 class Profile < ActiveRecord::Base
-
   belongs_to :user
 
   attr_accessible :full_name, :date_of_birth, :country_code, :state_code, :city, :address, :email, :phone, :skype, :description
 
   validates :full_name, allow_blank: true, format: /\A[A-z\s\-\.]+\z/
+
+  def description=(value)
+    write_attribute(:description, Loofah.fragment(read_attribute(:description)).scrub!(:strip).to_s)
+  end
 end
