74f4cd0d7f2773ee8521a586d3e64c72e1716669
Juri Semjonov <juri.semjonov@gmail.com>
2012-11-03 20:59:16 +0200

    added button 'go to top'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def go_to_top_link
+    content_tag :div, class: 'pull-right' do
+      link_to '#top', onclick: 'function(){window.scroll(); return false;}' do
+        'Go to top'
+      end
+    end
+  end
 end
